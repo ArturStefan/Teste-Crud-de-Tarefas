79ad4a34ada62218824f60c16156ac9ff74c85d3
Artur Stefan <arthurstephan13@gmail.com>
2020-03-13 10:28:58 -0300

    Criação do form

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from .models import *
+from .forms import *
 
 def index(request):
-    return render(request, 'tarefas/lista.html')
+
+    tarefas = Tarefa.objects.all()
+    
+    form = TarefaForm()
+
+    if request.method == "POST":
+        form = TarefaForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        
+        return redirect('/')
+
+    context = {'tarefas': tarefas,'form':form} 
+
+    return render(request, 'tarefas/lista.html', context)
