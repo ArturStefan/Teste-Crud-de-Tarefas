68ef2b45cbbd525a87d78638073dcfd85813cc2f
Artur Stefan <arthurstephan13@gmail.com>
2020-03-13 17:40:54 -0300

    Criando novos usuario

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=50)
     completa = models.BooleanField(default= False)
     criacao = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
