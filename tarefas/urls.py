26a38284c0dec3b8a9feeee006facae8af5ae48d
Artur Stefan <arthurstephan13@gmail.com>
2020-03-13 13:48:17 -0300

    Criando tela de login

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name="lista_tarefas"),
+    path('', views.index, name="lista-tarefas"),
     path('atualizar_tarefa/<str:pk>/', views.atualizarTarefa, name="atualizar-tarefa"),
     path('deletar_tarefa/<str:pk>/', views.deletarTarefa, name="deletar-tarefa"),
-    
+    path('registro/', include('django.contrib.auth.urls'))
 ]
